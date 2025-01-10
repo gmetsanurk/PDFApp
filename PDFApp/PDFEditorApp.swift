58c1574def8c3db11f13eee2af6cc67c04def145
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-01-10 12:36:10 +0300

    do setup Realm

--- PDFApp/PDFEditorApp.swift
+++ PDFApp/PDFEditorApp.swift
@@ -1,10 +1,26 @@
 import SwiftUI
+import RealmSwift
 
 @main
 struct PDFEditorApp: App {
+    init() {
+        setupRealm()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+    
+    private func setupRealm() {
+        let config = Realm.Configuration(
+            schemaVersion: 1,
+            migrationBlock: { migration, oldSchemaVersion in
+                if oldSchemaVersion < 1 {
+                }
+            }
+        )
+        Realm.Configuration.defaultConfiguration = config
     }
 }
